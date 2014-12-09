3ffb2660db92e0266ecbf48f3fd8cbacc4e71b8f
Timon Vonk <timonv@gmail.com>
2014-12-09 10:16:43 +0100

    Refactor to server struct

--- src/main.rs
+++ src/main.rs
@@ -2,74 +2,111 @@ use std::io::{TcpListener, TcpStream};
 use std::io::{Acceptor, Listener};
 use std::io::{BufferedStream, BufferedWriter};
 use std::sync::{RWLock, Arc};
+use std::collections::HashMap;
 
 
 // TODO
-// * Dont see own chat messages
-// * Refactor to ideomatic rust
+// * Refactor To 'Client' structs
+// * Clean up unwraps
+// * Experiment with a single dispatch flow setup:
+// Client -> Dispatcher -> Server
+//  ^-----------------------|
+// * Cleanup to files
+// * Tests :')
+// * Status displays
+// * Better UI
+// * Commands
 // * Timestamps
-// * Print list of connected clients to each
 
 // MAYBE
 // * Encryption
 // * Distributed setup
+
+type SharedUserStreamMap = Arc<RWLock<HashMap<String, TcpStream>>>;
+
+struct ChatServer {
+    users: SharedUserStreamMap
+}
+
+impl ChatServer {
+    pub fn new() -> ChatServer {
+        ChatServer { users: Arc::new(RWLock::new(HashMap::<String, TcpStream>::new())) }
+    }
+
+    pub fn register_user(&self, name: String, stream: TcpStream) {
+        let mut streams = self.users.write();
+        streams.insert(name, stream);
+        streams.downgrade();
+    }
+
+    pub fn listen_and_broadcast(&self, rec: Receiver<(String, String)>) {
+        let copy = self.clone();
+        spawn(proc() {
+            loop {
+                let (user, message) = rec.recv();
+                copy.broadcast(user, message);
+            }
+        })
+    }
+
+    fn broadcast(&self, origin: String, message: String) {
+        let message = self.format_message(&origin, &message);
+        let users = self.users.clone();
+        let unlocked = users.read();
+
+        for (user, stream) in unlocked.iter() {
+            if *user != origin { // Why?
+                let mut writer = BufferedWriter::new(stream.clone());
+                writer.write_line(message.as_slice()).unwrap();
+                writer.flush().unwrap();
+            }
+        }
+    }
+
+    fn format_message(&self, name: &String, message: &String) -> String {
+        format!("[{}]: {}", name, message.trim())
+    }
+}
+
+// TODO Remove need for this by doing everything over spawns with channels
+impl Clone for ChatServer {
+    fn clone(&self) -> ChatServer {
+        ChatServer { users: self.users.clone() }
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6667");
     let mut acceptor = listener.listen();
 
     // Used for passing chat messages between threads
-    let (tx, rx) = channel::<String>();
+    let (tx, rx) = channel::<(String, String)>();
+    let server = ChatServer::new();
 
-    // Vec needs typehint otherwise `cannot move out of dereference of `&`-pointer` errors
-    let streams_lock = Arc::new(RWLock::new(Vec::<TcpStream>::new()));
-    let streams_lock2 = streams_lock.clone(); // Clone before spawn
-
-    spawn(proc() {
-        loop {
-            let message: String = rx.recv(); //blocks
-            let streams = streams_lock2.read();
-
-            for stream in streams.iter() {
-                let mut writer = BufferedWriter::new(stream.clone());
-                writer.write_line(message.as_slice()).unwrap();
-                writer.flush().unwrap();
-                // Do loops clear scope per iter?
-            }
-        }
-    });
+    server.listen_and_broadcast(rx);
 
     for stream in acceptor.incoming() {
-        match stream {
-            Err(e) => { println!("Error {}", e); }
-            Ok(stream) => {
-                let mut streams = streams_lock.write();
-                streams.push(stream.clone());
-                streams.downgrade();
+        let txc = tx.clone();
+        let server_copy = server.clone();
 
-
-                // Clone before spawn
-                let txc = tx.clone();
-                spawn(proc() {
-                    handle_client(stream, txc);
-                });
-            }
-        }
+        // We can refactor this to channels, or encapsulate the clone, abstracting over
+        // concurrency, or both.
+        spawn(proc() {
+            handle_client(server_copy, stream.unwrap(), txc)
+        })
     }
 }
 
-fn handle_client(stream: TcpStream, tx: Sender<String>) {
+fn handle_client(server: ChatServer, stream: TcpStream, tx: Sender<(String, String)>) {
+    let mut copy = stream.clone();
     let mut stream = BufferedStream::new(stream);
     let name = get_name(&mut stream);
 
-    stream.write_line(format!("Your name is {}", name).as_slice()).unwrap();
-    stream.flush().unwrap();
+    server.register_user(name.clone(), copy);
 
     for line in stream.lines() {
-        // TODO Error handling + connection closing
-        let line = line.unwrap();
-        let message = format!("[{}]: {}", name, line.trim());
-        tx.send(message);
+        let data = (name.clone(), line.unwrap());
+        tx.send(data);
     }
 }
 
